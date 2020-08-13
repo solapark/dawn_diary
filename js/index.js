16216bb1f0f9f7b2ceb65e93ec3dc24f9645adfa
sola <sola@solas-Mac-mini.local>
2020-08-13 20:39:22 +0900

    calendar

--- js/index.js
+++ js/index.js
@@ -122,7 +122,6 @@ function showCalendar(){
         calendarBody.appendChild($tr);
     }
 }
-showCalendar();
 
 function removeCalendar(){
     let catchTr = 100;
@@ -133,4 +132,194 @@ function removeCalendar(){
     }
 }
 
+function prev(){
+    inputBox.value = "";
+    const $divs = document.querySelectorAll('#input-list > div');
+    $divs.forEach(function(e){
+      e.remove();
+    });
+    const $btns = document.querySelectorAll('#input-list > button');
+    $btns.forEach(function(e1){
+      e1.remove();
+    });
+    if(pageFirst.getMonth() === 1){
+        pageFirst = new Date(first.getFullYear()-1, 12, 1);
+        first = pageFirst;
+        if(first.getFullYear() % 4 === 0){
+            pageYear = leapYear;
+        }else{
+            pageYear = notLeapYear;
+        }
+    }else{
+        pageFirst = new Date(first.getFullYear(), first.getMonth()-1, 1);
+        first = pageFirst;
+    }
+    today = new Date(today.getFullYear(), today.getMonth()-1, today.getDate());
+    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;'+ first.getFullYear();
+    removeCalendar();
+    showCalendar();
+    showMain();
+    clickedDate1 = document.getElementById(today.getDate());
+    clickedDate1.classList.add('active');
+    clickStart();
+    reshowingList();
+}
+
+function next(){
+    inputBox.value = "";
+    const $divs = document.querySelectorAll('#input-list > div');
+    $divs.forEach(function(e){
+      e.remove();
+    });
+    const $btns = document.querySelectorAll('#input-list > button');
+    $btns.forEach(function(e1){
+      e1.remove();
+    });
+    if(pageFirst.getMonth() === 12){
+        pageFirst = new Date(first.getFullYear()+1, 1, 1);
+        first = pageFirst;
+        if(first.getFullYear() % 4 === 0){
+            pageYear = leapYear;
+        }else{
+            pageYear = notLeapYear;
+        }
+    }else{
+        pageFirst = new Date(first.getFullYear(), first.getMonth()+1, 1);
+        first = pageFirst;
+    }
+    today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
+    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;'+ first.getFullYear();
+    removeCalendar();
+    showCalendar();
+    showMain();
+    clickedDate1 = document.getElementById(today.getDate());
+    clickedDate1.classList.add('active');
+    clickStart();
+    reshowingList();
+}
+
+function showMain(){
+    mainTodayDay.innerHTML = dayList[today.getDay()];
+    mainTodayDate.innerHTML = today.getDate();
+}
+
+function clickStart(){
+    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+        tdGroup[i] = document.getElementById(i);
+        tdGroup[i].addEventListener('click',changeToday);
+    }
+}
+function changeToday(e){
+    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+        if(tdGroup[i].classList.contains('active')){
+            tdGroup[i].classList.remove('active');
+        }
+    }
+    clickedDate1 = e.currentTarget;
+    clickedDate1.classList.add('active');
+    today = new Date(today.getFullYear(), today.getMonth(), clickedDate1.id);
+    showMain();
+    keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
+    reshowingList();
+}
+
+function reshowingList(){
+    keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
+    if(todoList[keyValue] === undefined){
+        inputList.textContent = '';
+        todoList[keyValue] = [];
+        const $divs = document.querySelectorAll('#input-list > div');
+        $divs.forEach(function(e){
+          e.remove();
+        });
+        const $btns = document.querySelectorAll('#input-list > button');
+        $btns.forEach(function(e1){
+          e1.remove();
+        });
+    }else if(todoList[keyValue].length ===0){
+        inputList.textContent = "";
+        const $divs = document.querySelectorAll('#input-list > div');
+        $divs.forEach(function(e){
+          e.remove();
+        });
+        const $btns = document.querySelectorAll('#input-list > button');
+        $btns.forEach(function(e1){
+          e1.remove();
+        });
+    }else{
+        const $divs = document.querySelectorAll('#input-list > div');
+        $divs.forEach(function(e){
+          e.remove();
+        });
+        const $btns = document.querySelectorAll('#input-list > button');
+        $btns.forEach(function(e1){
+          e1.remove();
+        });
+        var $div = document.createElement('div');
+        for(var i = 0; i < todoList[keyValue].length; i++){
+            var $div = document.createElement('div');
+            $div.textContent = '-' + todoList[keyValue][i];
+            var $btn = document.createElement('button');
+            $btn.setAttribute('type', 'button');
+            $btn.setAttribute('id', 'del-ata');
+            $btn.setAttribute('id', dataCnt+keyValue);
+            $btn.setAttribute('class', 'del-data');
+            $btn.textContent = delText;
+            inputList.appendChild($div);
+            inputList.appendChild($btn);
+            $div.addEventListener('click',checkList);
+            $btn.addEventListener('click',deleteTodo);
+            inputBox.value = '';
+            function deleteTodo(){
+                $div.remove();
+                $btn.remove();
+            }
+        }
+    }
+
+}
+
+function addTodoList(){
+    var $div = document.createElement('div');
+    $div.textContent = '-' + inputBox.value;
+    var $btn = document.createElement('button');
+    $btn.setAttribute('type', 'button');
+    $btn.setAttribute('id', 'del-ata');
+    $btn.setAttribute('id', dataCnt+keyValue);
+    $btn.setAttribute('class', "del-data");
+    $btn.textContent = delText;
+    inputList.appendChild($div);
+    inputList.appendChild($btn);
+    todoList[keyValue].push(inputBox.value);
+    dataCnt++;
+    inputBox.value = '';
+    $div.addEventListener('click',checkList);
+    $btn.addEventListener('click',deleteTodo);
+    function deleteTodo(){
+        $div.remove();
+        $btn.remove();
+    }
+}
+function checkList(e){
+    e.currentTarget.classList.add('checked');
+}
+
 app.initialize();
+showCalendar();
+var clickedDate1 = document.getElementById(today.getDate());
+clickedDate1.classList.add('active');
+var prevBtn = document.getElementById('prev');
+var nextBtn = document.getElementById('next');
+prevBtn.addEventListener('click',prev);
+nextBtn.addEventListener('click',next);
+var tdGroup = [];
+var inputBox = document.getElementById('input-box');
+var inputDate = document.getElementById('input-data');
+var inputList = document.getElementById('input-list');
+var delText = 'X';
+inputDate.addEventListener('click',addTodoList);
+var dataCnt = 1;
+var keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
+let todoList = [];
+todoList[keyValue] = [];
+console.log(keyValue);
